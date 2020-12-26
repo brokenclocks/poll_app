d282a89560957ed979a62fe5bb48347ea1d74c57
brokenclocks <bernardleigh94@gmail.com>
2020-12-26 11:29:03 -0600

    pagination almost done

--- django_Profile_Blog/blog/models.py
+++ django_Profile_Blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,6 +10,8 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
 
-def __str__(self):
-    return self.title
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- django_Profile_Blog/users/models.py
+++ django_Profile_Blog/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import image
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -14,3 +14,11 @@ class Profile(models.Model):
 
     def save(self):
         super().save()
+        # save image and resize it at the same time
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+
+            img.thumbnail(output_size)
+            img.save(self.image.path)
